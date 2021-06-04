1d51e716beecbcecb41efcfbf652796cc569c770
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-06-04 18:56:59 -0300

    .

--- win_installApps.ps1
+++ win_installApps.ps1
@@ -1,3 +1,5 @@
+# Dependencies: https://github.com/microsoft/winget-cli/
+
 winget install --id=Python.Python -e  ; 
 winget install --id=Microsoft.VisualStudioCode -e  ; 
 winget install --id=BraveSoftware.BraveBrowser -e  ; 
@@ -7,4 +9,7 @@ winget install --id=Discord.Discord -e  ;
 winget install --id=AntibodySoftware.WizTree -e  ; 
 winget install --id=Valve.Steam -e ; 
 winget install --id=Microsoft.WindowsTerminal -e  ; 
-winget install --id=PuTTY.PuTTY -e 
+winget install --id=PuTTY.PuTTY -e ; 
+winget install --id=7zip.7zip -e ;
+winget install --id=Git.Git -e  ; 
+winget install --id=VMware.WorkstationPlayer -e 
