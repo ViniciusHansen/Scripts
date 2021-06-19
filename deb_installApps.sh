9aae0822e8daaf1d18401011eee60fdf316a9008
Vinícius Hansen <vinicius.carlo.hansen@gmail.com>
2021-06-19 07:49:54 -0700

    Update deb_installApps.sh

--- deb_installApps.sh
+++ deb_installApps.sh
@@ -13,20 +13,18 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak update 
 
 #Apps Install
-curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
-sudo apt install neofetch -y
-sudo apt install htop -y
-sudo apt install git -y
-sudo apt install speedtest -y
-sudo apt install libreoffice -y
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub org.gimp.GIMP -y
-flatpak install flathub com.visualstudio.code -y
-
-#brave browser
-sudo apt install apt-transport-https curl
+sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install brave-browser -y
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt install speedtest -y
+sudo apt install neofetch -y
+sudo apt install htop -y
+sudo apt install git -y
+sudo apt install libreoffice -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.visualstudio.code -y
+
