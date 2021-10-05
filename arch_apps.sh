9fe727ec72a33e16434ccdd271bc756713995cb8
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-10-05 15:18:25 -0300

    add pip & docker

--- arch_apps.sh
+++ arch_apps.sh
@@ -4,7 +4,7 @@ setxkbmap -model abnt2 -layout br
 
 sudo pacman -Syu
 
-sudo pacman -S libinput chromium firefox gimp libreoffice code thunderbird discord steam git spotify htop neofetch
+sudo pacman -S libinput chromium firefox gimp libreoffice code thunderbird discord steam git htop neofetch python-pip docker docker-compose
 
 sudo pacman -Scc
 
