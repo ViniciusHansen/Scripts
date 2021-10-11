1be8b01619fe71c89e58416bec0889d246b6d63b
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2021-10-11 14:38:16 -0300

    add more apps

--- arch_apps.sh
+++ arch_apps.sh
@@ -4,7 +4,7 @@ setxkbmap -model abnt2 -layout br
 
 sudo pacman -Syu
 
-sudo pacman -S libinput chromium firefox gimp libreoffice code thunderbird discord steam git htop neofetch python-pip docker docker-compose
+sudo pacman -S libinput chromium firefox gimp libreoffice code thunderbird discord steam git htop neofetch python-pip docker docker-compose bpytop ffmpeg base-devel vim obs-studio kdenlive noto-fonts-emoji nodejs okular radeontop 
 
 sudo pacman -Scc
 
